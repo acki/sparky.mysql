438679fdcc1d671b5aebce08c517818fe41ae807
christoph.ackermann <info@acki.be>
2011-10-10 11:27:53 +0200

    Worked on connector and error handling

--- class.sparky.mysql.php
+++ class.sparky.mysql.php
@@ -1,19 +1,41 @@
 <?php
 
+   /**
+    * Class for creating MySQL connections and queries
+    */
+
     class mysql {
     
+        // variables
         public     $mysqli;
     
+        // construct function, called when creating a new object
         function __construct() {
         
+            // globalize some data
             global $cfg;
-        
-            if($this->mysqli = new mysqli($cfg['mysql']['host'], $cfg['mysql']['user'], $cfg['mysql']['password'], $cfg['mysql']['database']) && isset($this->mysqli)) {
+            
+            // create a connection. @ means suppressing errors (we handle it self)
+            $this->mysqli = @new mysqli($cfg['mysql']['host'], $cfg['mysql']['user'], $cfg['mysql']['password'], $cfg['mysql']['database']);
+            
+            // if connection is good
+            if(!mysqli_connect_error() && is_object($this->mysqli)) {
+                
+                // return mysqli object
                 return $this->mysqli;
+                
+            // else error handling
             } else {
-                print mysqli_connect_error();
-                print "doof";
-                //panic('No MySQL connection!');
+            
+                // check if mysql error
+                $error = mysqli_connect_error();
+                if(!mysqli_connect_error()) {
+                    $error = 'We got no database object! Don\'t know why...';
+                }
+                
+                // throw error
+                panic($error);
+                
             }
             
         }
