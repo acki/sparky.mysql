d42564c2647d77b0b59c4f9815c5f9d521b5054f
christoph.ackermann <info@acki.be>
2011-10-10 10:26:01 +0200

    Added first mysqli connection and some debug messages

--- class.sparky.mysql.php
+++ class.sparky.mysql.php
@@ -2,10 +2,19 @@
 
     class mysql {
     
-        function getHello() {
+        public     $mysqli;
+    
+        function __construct() {
         
-            print "hello";
-            print "hello2";
+            global $cfg;
+        
+            if($this->mysqli = new mysqli($cfg['mysql']['host'], $cfg['mysql']['user'], $cfg['mysql']['password'], $cfg['mysql']['database']) && isset($this->mysqli)) {
+                return $this->mysqli;
+            } else {
+                print mysqli_connect_error();
+                print "doof";
+                //panic('No MySQL connection!');
+            }
             
         }
         
